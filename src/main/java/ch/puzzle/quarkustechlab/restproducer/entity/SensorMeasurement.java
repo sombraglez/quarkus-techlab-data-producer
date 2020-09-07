ee7ac44e75e1099bd1a628dd66d424848ce4aaca
Sombra González <gonzalez@puzzle.ch>
2020-09-07 14:55:41 +0200

    Add source code for producer

---
+++ src/main/java/ch/puzzle/quarkustechlab/restproducer/entity/SensorMeasurement.java
@@ -0,0 +1,11 @@
+package ch.puzzle.quarkustechlab.restproducer.entity;
+
+public class SensorMeasurement {
+
+    public Double data;
+
+    public SensorMeasurement() {
+        this.data = Math.random();
+    }
+}
+
